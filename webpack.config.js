92b98997c94dce04b287df7eb99285eb58c44ce1
wat-aro <kazutas1008@gmail.com>
2018-09-17 01:57:16 +0900

    The Manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: {
@@ -9,12 +10,13 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    new ManifestPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].bundle[hash].js',
     path: path.resolve(__dirname, 'dist')
   }
 }
